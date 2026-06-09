76c4e39fdf37df62ca4acc6a42af6a3542bc9d83
Robert-corzo <robert.mg.corzo@gmail.com>
2026-06-08 19:25:40 -0500

    Ejercicio

--- backtracking1.py
+++ backtracking1.py
@@ -4,16 +4,13 @@ def imprimir(mat):
     print()
 
 def validar(lab, f, c, res):
-    # Verificar límites
     if f < 0 or f >= len(lab) or c < 0 or c >= len(lab[0]):
         return False
-    # Verificar si es pared (0) o ya visitada (1)
     if lab[f][c] == 0 or res[f][c] == 1:
         return False
     return True
 
 def laberinto(lab, res, f, c, fin_f, fin_c):
-    # Caso base: Llegamos a la meta
     if f == fin_f and c == fin_c:
         res[f][c] = 1
         imprimir(res)
@@ -23,13 +20,11 @@ def laberinto(lab, res, f, c, fin_f, fin_c):
         res[f][c] = 1
         imprimir(res) # Muestra el avance
         
-        # Intentar las 4 direcciones: Abajo, Derecha, Arriba, Izquierda
         movimientos = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         for df, dc in movimientos:
             if laberinto(lab, res, f + df, c + dc, fin_f, fin_c):
                 return True
         
-        # Backtracking: Si no funcionó, marcamos como 0 y mostramos el retroceso
         res[f][c] = 0
         imprimir(res)
         return False
@@ -51,7 +46,6 @@ lab = [
 
 res = [[0 for _ in range(9)] for _ in range(9)]
 
-# Inicio (0,0), Fin (8,8)
 if laberinto(lab, res, 0, 0, 8, 8):
     print("¡Ruta encontrada!")
 else:
